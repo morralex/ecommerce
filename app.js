705c0b542e4e08695900416f4761544f1773c0da
morralex <morr.alex@outlook.com>
2020-09-29 02:19:13 -0700

    adding order info

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const categoryRoutes = require('./routes/category');
 const productRoutes = require('./routes/product');
-const braintreeRoutes = require('./routes/braintree')
+const braintreeRoutes = require('./routes/braintree');
+const orderRoutes = require('./routes/order');
 
 // app
 const app = express();
@@ -26,7 +27,7 @@ mongoose
 
 //middlewares
 app.use(morgan('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
 app.use(cookieParser());
 app.use(expressValidator());
 app.use(cors());
@@ -37,6 +38,7 @@ app.use('/api', userRoutes);
 app.use('/api', categoryRoutes);
 app.use('/api', productRoutes);
 app.use('/api', braintreeRoutes);
+app.use('/api', orderRoutes);
 
 const port = process.env.PORT || 8000
 
